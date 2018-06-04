4be90bbfbfc17bff5ebcfbe07907cecb5867f97d
Robert Ramsay <rramsay@telesign.com>
2018-06-04 14:27:59 -0700

    Merge the standard sdk clients into the enterprise SDK, including AppVerify, Messaging, PhoneId, Score, and Voice. Also bump the version of the standard sdk dependency

---
+++ TelesignEnterprise/AppVerifyClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using _AppVerifyClient = Telesign.AppVerifyClient;
+
+namespace TelesignEnterprise
+{
+    public class AppVerifyClient : _AppVerifyClient
+    {
+        public AppVerifyClient(string customerId,
+            string apiKey)
+            : base(customerId,
+                apiKey,
+                "https://rest-ww.telesign.com")
+        { }
+
+        public AppVerifyClient(string customerId,
+            string apiKey,
+            string restEndpoint)
+            : base(customerId,
+                apiKey,
+                restEndpoint)
+        { }
+
+        public AppVerifyClient(string customerId,
+            string apiKey,
+            string restEndpoint,
+            int timeout,
+            WebProxy proxy,
+            string proxyUsername,
+            string proxyPassword)
+            : base(customerId,
+                apiKey,
+                restEndpoint,
+                timeout: timeout,
+                proxy: proxy,
+                proxyUsername: proxyUsername,
+                proxyPassword: proxyPassword)
+        { }
+    }
+}
