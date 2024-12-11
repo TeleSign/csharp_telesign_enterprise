ac86c69b5678157d3907c39bc060ce72be7181b1
Leonardo Aponte <vaponte@telesign.com>
2024-12-11 16:47:40 -0500

    Add new class example of new method Verify API

---
+++ TelesignEnterprise.Example/VerifyOmnichannel/VerificationSmsOwnOTP.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Telesign;
+
+namespace TelesignEnterprise.Example.VerifyOmnichannel
+{
+    class VerificationSmsOwnOTP
+    {
+        static void Main(string[] args)
+        {
+            string customerId = "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
+            string apiKey = "EXAMPLETE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
+
+            string phoneNumber = "phone_number";
+            string verifyCode = "12345";
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("security_factor", verifyCode);
+
+            try
+            {
+                VerifyClient verifyClient = new VerifyClient(customerId, apiKey);
+                RestClient.TelesignResponse telesignResponse = verifyClient.CreateVerificationProcess(phoneNumber, parameters);
+
+                Console.WriteLine("Please enter your verification code:");
+                string code = Console.ReadLine().Trim();
+
+                if (verifyCode == code)
+                {
+                    Console.WriteLine("Your code is correct.");
+                }
+                else
+                {
+                    Console.WriteLine("Your code is incorrect.");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
